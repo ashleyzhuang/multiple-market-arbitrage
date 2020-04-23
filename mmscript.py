0c8364d521438420b2bd2e1292a49968ddfeea07
Ashley Zhuang <ashleyzhuang01@gmail.com>
2020-04-22 17:53:27 -0700

    Updated script

--- mmscript.py
+++ mmscript.py
@@ -1,52 +1,119 @@
 import requests
 
-response = requests.get('https://www.predictit.org/api/marketdata/all/')
-data = response.json()
+cleaned_marks = []
 
-markets = data['markets']
-cleaned = []
-for i in range(len(markets)):
-    consol = {'name': markets[i]['name'], 'cont': []}
-    contracts = markets[i]['contracts']
-    for j in range(len(contracts)):
-        if contracts[j]['status'] == 'Open':
-            temp = {'cont_name': contracts[j]['shortName'], 'Buy Yes': contracts[j]['bestBuyYesCost'], 'Buy No': contracts[j]['bestBuyNoCost'], 'Sell Yes': contracts[j]['bestSellYesCost'], 'Sell No': contracts[j]['bestSellNoCost']}
-            consol['cont'].append(temp)
-    cleaned.append(consol)
-# buying no's strategy
-possible_no = []
-for mark in cleaned:
+# Loads market data from PredictIt API
+def load_data():
+    response = requests.get('https://www.predictit.org/api/marketdata/all/')
+    data = response.json()
+
+    markets = data['markets']
+    for i in range(len(markets)):
+        consol = {'id': markets[i]['id'], 'name': markets[i]['name'], 'cont': []}
+        contracts = markets[i]['contracts']
+        for j in range(len(contracts)):
+            if contracts[j]['status'] == 'Open':
+                temp = {'cont_name': contracts[j]['shortName'], 'Buy Yes': contracts[j]['bestBuyYesCost'], 'Buy No': contracts[j]['bestBuyNoCost'], 'Sell Yes': contracts[j]['bestSellYesCost'], 'Sell No': contracts[j]['bestSellNoCost']}
+                consol['cont'].append(temp)
+        cleaned_marks.append(consol)
+
+# Detects arbitrage for a given market
+# Returns tuple of minimum profit for buying all no's and buying all yes's
+def detect_arb(mark):
     contracts = mark['cont']
-    tot = []
+    prices_no = []
+    prices_yes = []
+    profit_no = None
+    profit_yes = None
+
     for cont in contracts:
         if cont['Buy No'] != None:
-            tot.append(cont['Buy No'])
-    if tot:
-        loss = min(tot)
-        profit = 0
-        for i in range(len(tot)):
-            if tot[i] == loss:
-                continue
-            profit += 1 - tot[i]
-        if profit >= loss:
-            possible_no.append((mark, profit-loss))
-possible_no.sort(key = lambda x: x[1], reverse=True)
-print(possible_no)
-
-    
-# buying yes' strategy
-possible_yes = []
-for mark in cleaned:
-    contracts = mark['cont']
-    tot = []
-    if len(contracts) == 1:
-        continue
-    for cont in contracts:
+            prices_no.append(cont['Buy No'])
         if cont['Buy Yes'] != None:
-            tot.append(cont['Buy Yes'])
-    if tot:
-        cost = sum(tot)
-        if cost <= 1:
-            possible_yes.append((mark, 1 - cost))
-possible_yes.sort(key = lambda x: x[1], reverse=True)
-print(possible_yes)
+            prices_yes.append(cont['Buy Yes'])
+    if prices_no:
+        loss = sum(prices_no)
+        gain = len(prices_no) - 1
+        profit_no = round(gain - loss, 2)
+    if prices_yes:
+        cost = sum(prices_yes)
+        profit_yes = round(1 - cost, 1)
+
+    return profit_no, profit_yes
+
+# Prints arbitrage report for a single market
+def print_arb(mark):
+    profit_no, profit_yes = detect_arb(mark)
+
+    print('------------------------------------------------------------------')
+    print('Market:', mark['name'], '\nID:', mark['id'])
+    print()
+
+    if profit_no and profit_no > 0:
+        print('Buy No\'s: ARBITRAGE FOUND! :D')
+    else:
+        print('Buy No\'s: No Arbitrage :(')
+
+    print('Minimum profit: ', profit_no)
+    print()
+
+    if profit_yes and profit_yes > 0:
+        print('Buy Yes\'s: ARBITRAGE FOUND! :D')
+    else:
+        print('Buy Yes\'s: No Arbitrage :(')
+
+    print('Minimum profit: ', profit_yes)
+    print('------------------------------------------------------------------')
+
+# Prints out all markets with arbitrage
+def all_arbs():
+    total = 0
+
+    arb_no = []
+    arb_yes = []
+
+    for mark in cleaned_marks:
+        profit_no, profit_yes = detect_arb(mark)
+        if profit_no and profit_no > 0:
+            arb_no.append((mark, profit_no))
+        if profit_yes and profit_yes > 0:
+            arb_no.append((mark, profit_yes))
+
+    arb_no.sort(key = lambda x: x[1], reverse=True)
+    arb_yes.sort(key = lambda x: x[1], reverse=True)
+
+    for tup in arb_no:
+        mark = tup[0]
+        profit = tup[1]
+        print('ID:', mark['id'], '\nMarket:', mark['name'], )
+        print('Buying no\'s min profit:', profit)
+        print()
+
+    for tup in arb_yes:
+        mark = tup[0]
+        profit = tup[1]
+        print('Market:', mark['name'], '\nID:', mark['id'])
+        print('Buying yes\'s min profit:', profit)
+        print()
+
+    print('TOTAL instances of arbitrage:', len(arb_no) + len(arb_yes))
+
+
+load_data()
+
+# Main execution
+while True:
+    user_input = input('Market ID (-1 to exit, "all" for all markets): ')
+    if user_input == 'all':
+        all_arbs()
+    else:
+        market_id = int(user_input)
+
+        if market_id == -1:
+            break
+        mark = next((m for m in cleaned_marks if m['id'] == market_id), None)
+
+        if mark == None:
+            print("Invalid Market ID")
+        else:
+            print_arb(mark)
